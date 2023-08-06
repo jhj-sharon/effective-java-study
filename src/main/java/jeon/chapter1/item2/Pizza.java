817eb3e19f41a7c4b0fd54da056432fb04d77f27
sharon <jhj.sharon@gmail.co>
2023-08-06 12:53:58 +0900

    refactor : [item2] 생성자에 매개변수가 많다면 빌더를 고려하라

---
+++ src/main/java/jeon/chapter1/item2/Pizza.java
@@ -0,0 +1,63 @@
+package jeon.chapter1.item2;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public abstract class Pizza {
+    public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+        public T addTopping(Topping topping) {
+            toppings.add(topping);
+            return self();
+        }
+
+        abstract Pizza build();
+
+        // Subclasses must override this method to return "this"
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
+}
+
+class NyPizza extends Pizza {
+    public enum Size { SMALL, MEDIUM, LARGE }
+    private final Size size;
+
+    static class Builder extends Pizza.Builder<Builder> {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = size;
+        }
+
+        @Override
+        public NyPizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE)
+                .addTopping(Pizza.Topping.ONION)
+                .build();
+    }
+}
+
